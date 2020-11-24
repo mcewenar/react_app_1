2342f65ab752acaf23d448c0d751fc40bd70c69c
David McEwen <dmcewena@hotmail.com>
2020-11-23 21:25:37 -0500

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React, { Components } from 'react';
+//import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-
+//this importation is necessary
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Hello World </NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
     </div>
   );
 }
