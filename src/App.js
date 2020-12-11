fa42002bce86a375d8a581c3f909fdad4c131caa
David McEwen <dmcewena@hotmail.com>
2020-12-10 23:44:27 -0500

    Components part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Components } from 'react';
+import Menu from './components/MenuComponent'; //export component within other component.
 //import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
-//this importation is necessary
+//this importation is necessary. JSX
 function App() {
   return (
     <div className="App">
       <div className="App">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Hello World </NavbarBrand>
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     </div>
+    
   );
 }
 
