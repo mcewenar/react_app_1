82aacc7b729d741e20eb075c09f7945d87519923
David McEwen <dmcewena@hotmail.com>
2020-12-14 16:16:32 -0500

    Components part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { Components } from 'react';
-import Menu from './components/MenuComponent'; //export component within other component.
-//import logo from './logo.svg';
+import React, { Component } from 'react';
+import Menu from './components/MenuComponent'; //export component within other component
+import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import { DISHES } from './shared/dishes';
 //this importation is necessary. JSX
-function App() {
+/*function App() {
   return (
     <div className="App">
       <div className="App">
@@ -20,4 +21,27 @@ function App() {
   );
 }
 
+export default App;  OTHER FORM USING FUNCTIONS*/
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
+}
 export default App;
